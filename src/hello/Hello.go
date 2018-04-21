95fc7ed417a0b12dfbb3e2acc18882f75ab135ae
Adrian Moseley <adrian.moseley@gmail.com>
2018-04-21 08:19:20 -0500

    Clean up var declarations

--- src/hello/Hello.go
+++ src/hello/Hello.go
@@ -13,16 +13,16 @@ func add(x int, y int) int {
 }
 */
 func main() {
-	var MyFirstName string = "Adrian"
-	var MyLastName string = "Moseley"
-	var MyAge int = 41
-	var MyYear int = 1976
+	var MyFirstName = "Adrian"
+	var MyLastName = "Moseley"
+	var MyAge = 41
+	var MyYear = 1976
 	fmt.Printf("First Name: %s\nLast Name: %s\nAge:%d\nBirth Year: %d\n", MyFirstName, MyLastName, MyAge, MyYear)
 	fmt.Printf("Hello, world\n")
 	fmt.Printf("Hello, Adrian!\n")
 	fmt.Println(moseleyutils.Moseley_add(3, 4))
 	fmt.Printf(stringutil.Reverse("!oG ,olleH"))
-	var MoseleyADD int = moseleyutils.Moseley_add(10, 11)
+	var MoseleyADD = moseleyutils.Moseley_add(10, 11)
 
 	fmt.Printf("Adding the Moseley Func of 10 + 20 is %d", MoseleyADD)
 	/*
